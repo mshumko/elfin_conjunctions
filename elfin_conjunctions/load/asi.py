1bb368cbf07a22f5a50366e7c11bd516c7f778b9
Mykhaylo Shumko <msshumko@gmail.com>
2022-03-28 12:09:25 -0400

    Added asi_locations.csv and working on the data loaders.

---
+++ elfin_conjunctions/load/asi.py
@@ -0,0 +1,20 @@
+import pathlib
+
+import pandas as pd
+
+import elfin_conjunctions.config as config
+
+def load_locations(array=None, location_code=None):
+    """
+    Loads the asi_locations.csv file.
+
+    Parameters
+    ----------
+    array: str
+        Optionally filter by the ASI array.
+    location_code: str
+        Optionally filter by the ASI location_code.
+    """
+
+    return
+
---
+++ elfin_conjunctions/load/elfin.py
@@ -0,0 +1,7 @@
+# The ELFIN data loaders
+
+def ephemeirs(sc_id, day):
+    raise NotImplementedError
+
+def epd(sc_id, day):
+    raise NotImplementedError
