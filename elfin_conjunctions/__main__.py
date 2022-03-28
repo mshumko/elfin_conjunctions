89b2f912e73dbe53d5c98d77881a8cf46b3bb62a
Mykhaylo Shumko <msshumko@gmail.com>
2022-03-28 12:00:32 -0400

    Updating the template.

--- elfin_conjunctions/__main__.py
+++ elfin_conjunctions/__main__.py
@@ -5,40 +5,29 @@ import configparser
 import elfin_conjunctions
 
 # Run the configuration script with
-# python3 -m project [init, initialize, config, or configure]
+# python3 -m elfin_asi_conjunctions [init, initialize, config, or configure]
 
 here = pathlib.Path(__file__).parent.resolve()
 
 if (len(sys.argv) > 1) and (sys.argv[1] in ['init', 'initialize', 'config', 'configure']):
     print('Running the configuration script.')
-    s = (
-        f'What is your project data directory? Press enter for the default '
-        f'directory at ~/project/ folder will be created.\n'
-    )
-    project_data_dir = input(s)
+    s = f'Where is your ELFIN data directory?'
+    elfin_data_dir = input(s)
 
-    # If the user specified the directory, check that the directory already exists
-    # and make that directory if it does not.
-    if project_data_dir != '':
-        if not pathlib.Path(project_data_dir).exists():
-            pathlib.Path(project_data_dir).mkdir(parents=True)
-            print(f'Made project data directory at {pathlib.Path(project_data_dir)}.')
+    if elfin_data_dir != '':
+        if not pathlib.Path(elfin_data_dir).exists():
+            pathlib.Path(elfin_data_dir).mkdir(parents=True)
+            print(f'Made ELFIN data directory at {pathlib.Path(elfin_data_dir)}.')
         else:
-            print(f'The project data directory at {pathlib.Path(project_data_dir)} already exists.')
+            print(f'The ELFIN data directory at {pathlib.Path(elfin_data_dir)} already exists.')
     else:
-        # If the user did not specify the directory, make one at ~/project/.
-        project_data_dir = pathlib.Path.home() / 'project'
-        if not project_data_dir.exists():
-            project_data_dir.mkdir(parents=True)
-            print(f'project directory at {project_data_dir} created.')
-        else:
-            print(f'project directory at {project_data_dir} already exists.')
+        raise ValueError('No ELFIN directory was provided.')
 
     # Create a configparser object and add the user configuration.
     config = configparser.ConfigParser()
     config['Paths'] = {
         'project_dir':here,
-        'project_data_dir': project_data_dir
+        'elfin_data_dir': elfin_data_dir
         }
 
     with open(here / 'config.ini', 'w') as f:
@@ -47,6 +36,6 @@ if (len(sys.argv) > 1) and (sys.argv[1] in ['init', 'initialize', 'config', 'con
 else:
     print(
         'This is a configuration script to set up config.ini file. The config '
-        'file contains the project data directory, ~/project/ by '
-        'default. To configure this package, run python3 -m project config.'
+        'file contains the ELFIN data directory and the directory of the source '
+        'code. To configure this package, run python3 -m elfin_asi_conjunctions config.'
     )
