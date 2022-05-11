7082c8b937126aaf565fb63f5d2c7b1f07787649
Mykhaylo Shumko <msshumko@gmail.com>
2022-05-11 07:45:09 -0400

    Moved the conjunction merging code into the find_elin_themis_conjunctions.py

--- elfin_conjunctions/find_elfin_themis_conjunctions.py
+++ elfin_conjunctions/find_elfin_themis_conjunctions.py
@@ -93,8 +93,7 @@ for sc_id in ['a', 'b']:
                     conjunction_df.to_csv(save_path, index=False)
 
 
-    # Merge the individual files from each THEMIS ASI into one file for ELFIN-A and one file
-    # for ELFIN-B
+    # Finally merge the conjunction files with ELFIN-X into one.
     file_paths = save_dir.rglob(f'elfin_{sc_id.lower()}_themis_*_conjunctions.csv')
     merged_conjunctions = pd.DataFrame(columns=['start', 'end', 'epd_data', 'asi_data', 'asi'])
 
@@ -103,3 +102,15 @@ for sc_id in ['a', 'b']:
         df['asi'] = file_path.name.split('_')[3]
 
         merged_conjunctions = pd.concat([merged_conjunctions, df])
+
+    # Sort the conjunctions by time.
+    merged_conjunctions['start'] = pd.to_datetime(merged_conjunctions['start'])
+    merged_conjunctions = merged_conjunctions.sort_values('start')
+    merged_conjunctions.to_csv(save_dir.parents[0] / f'elfin_{sc_id.lower()}_themis_asi_conjunctions.csv', 
+        index=False)
+
+    filtered_conjunctions = merged_conjunctions[
+        (merged_conjunctions['asi_data'] == True) & (merged_conjunctions['epd_data'] == True)
+        ]
+    filtered_conjunctions.to_csv(save_dir.parents[0] / f'elfin_{sc_id.lower()}_themis_asi_conjunctions_filtered.csv', 
+        index=False)
